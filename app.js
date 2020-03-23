2b9ad87a4e9fc7fdbafc79342eebbfc6c4916c23
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2020-03-23 11:24:45 +0100

    w

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send('der Server functionieret')
 })
 
 
 app.listen(3000)
+
+module.exports = app;
